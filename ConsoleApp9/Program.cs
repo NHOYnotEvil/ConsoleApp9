1c755843ccb8c0009bf68e464f57b15cebc97b26
NHOY <NHOY@2210DR000919>
2023-05-04 00:34:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroName;
+            string baseName;
+            int heroLevel;
+            int heroExperienceForNextLevel;
+            int heroHealth;
+            int heroDamage;
+            int heroDefence;
+            int heroAttackSpeed;
+            int enemyHealth;
+            int enemyDamage;
+            int enemyDefence;
+            bool battleOrNot;
+        }
+    }
+}
